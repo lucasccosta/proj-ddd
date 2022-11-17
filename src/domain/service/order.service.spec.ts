aac1ba72d8d85d2fd38997464fe975478b9bd89d
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-16 22:01:01 -0300

    add order service and its test

---
+++ src/domain/service/order.service.spec.ts
@@ -0,0 +1,28 @@
+import Customer from "../domain/entity/customer";
+import Order from "../domain/entity/order";
+import OrderItem from "../domain/entity/order_item";
+import OrderService from "./order.service";
+
+describe("Order service unit tests", () => {
+  it("should place an order", () => {
+    const customer = new Customer("c1", "Customer 1");
+    const orderItem1 = new OrderItem("i1", "Item 1", 10, "p1", 1);
+
+    const order = OrderService.placeOrder(customer, [orderItem1]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
+  it("should get total of all orders", () => {
+    const orderItem1 = new OrderItem("i1", "Item 1", 100, "p1", 1);
+    const orderItem2 = new OrderItem("i2", "Item 2", 90, "p2", 2);
+
+    const order1 = new Order("o1", "c1", [orderItem1]);
+    const order2 = new Order("o2", "c2", [orderItem2]);
+
+    const total = OrderService.total([order1, order2]);
+
+    expect(total).toBe(280);
+  });
+});
