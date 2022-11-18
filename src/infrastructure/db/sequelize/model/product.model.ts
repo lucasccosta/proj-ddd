c87a829b8fd8c837f83eb9ae77ae7e4ba33e2792
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-18 15:09:52 -0300

    add product model

---
+++ src/infrastructure/db/sequelize/model/product.model.ts
@@ -0,0 +1,16 @@
+import { Table, Column, PrimaryKey, Model } from "sequelize-typescript";
+@Table({
+  tableName: "products",
+  timestamps: false,
+})
+export default class ProductModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare price: number;
+}
