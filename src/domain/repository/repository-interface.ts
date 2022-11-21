897e8bd258ee0a0d08d81ab7d55922280620c287
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-21 14:49:30 -0300

    creating customer repository interface and changing get_by_id to camelCase

--- src/domain/repository/repository-interface.ts
+++ src/domain/repository/repository-interface.ts
@@ -1,6 +1,6 @@
 export default interface RepositoryInterface<T> {
   create(entity: T): Promise<void>;
   update(entity: T): Promise<void>;
-  get_by_id(id: string): Promise<T>;
+  getById(id: string): Promise<T>;
   getAll(): Promise<T[]>;
 }
