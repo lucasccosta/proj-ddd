f56694f2a7601a96a7ac4cc6ffff895a04177e15
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-21 14:48:19 -0300

    add getters on address object

--- src/domain/entity/address.ts
+++ src/domain/entity/address.ts
@@ -1,31 +1,43 @@
 export default class Address {
+  _street: string = "";
+  _number: number = 0;
+  _zipcode: string = "";
+  _city: string = "";
 
-  _street: string = ''
-  _number: number = 0
-  _zip: string = ''
-  _city: string = ''
+  constructor(street: string, number: number, zipcode: string, city: string) {
+    this._street = street;
+    this._number = number;
+    this._zipcode = zipcode;
+    this._city = city;
 
-  constructor(street: string, number: number, zip: string, city: string) {
-    this._street = street
-    this._number = number
-    this._zip = zip
-    this._city = city
+    this.validate();
+  }
 
-    this.validate()
+  get street() {
+    return this._street;
+  }
+  get number() {
+    return this._number;
+  }
+  get zipcode() {
+    return this._zipcode;
+  }
+  get city() {
+    return this._city;
   }
 
   validate() {
     if (this._street.length === 0) {
-      throw new Error("Street is required")
+      throw new Error("Street is required");
     }
     if (this._number === 0) {
-      throw new Error("Number is required")
+      throw new Error("Number is required");
     }
-    if (this._zip.length === 0) {
-      throw new Error("Zip code is required")
+    if (this._zipcode.length === 0) {
+      throw new Error("zipcode code is required");
     }
     if (this._city.length === 0) {
-      throw new Error("City is required")
+      throw new Error("City is required");
     }
   }
 }
