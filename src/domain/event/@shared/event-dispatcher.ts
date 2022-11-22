887ffbc019d61941682b74fdd41aab3aa685a1ce
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-22 07:58:54 -0300

    creating event disppatcher

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,44 @@
+import EventDispatcherInterface from "./event-dispatcher.interface";
+import EventHandlerInterface from "./event-handler.interface";
+import eventHandlerInterface from "./event-handler.interface";
+import eventInterface from "./event.interface";
+
+export default class EventDispatcher implements EventDispatcherInterface {
+  // O eventName é uma string, e o tipo do eventHandlers é um array da eventHandlersInterfaces
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
+  notify(event: eventInterface): void {
+    // pega o evento baseado no nome da classe
+    const eventName = event.constructor.name;
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
+  }
+
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    // Nota mental: this.eventHandlers[eventName] == this.eventHandlers.eventName
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
+  }
+
+  unregister(eventName: string, eventHandler: eventHandlerInterface): void {
+    if (this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
+  }
+
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
+}
