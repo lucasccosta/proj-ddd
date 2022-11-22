7e9c35291fb3ff6206bbb6e5e35a93212ef29746
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-22 07:56:15 -0300

    add getters to parameteres

--- src/domain/entity/order.ts
+++ src/domain/entity/order.ts
@@ -1,31 +1,40 @@
-import OrderItem from "./order_item"
+import OrderItem from "./order_item";
 
 export default class Order {
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[] = [];
+  private _total: number;
 
-  private _id: string
-  private _customerId: string
-  private _items: OrderItem[] = []
-  private _total: number
-  
-  constructor(id: string, customerId: string, items: OrderItem[]){
-    this._id = id
-    this._customerId = customerId
-    this._items = items
-    this._total = this.total()
-    this.validate()
+  constructor(id: string, customerId: string, items: OrderItem[]) {
+    this._id = id;
+    this._customerId = customerId;
+    this._items = items;
+    this._total = this.total();
+    this.validate();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+  get customerId(): string {
+    return this._customerId;
+  }
+  get items(): OrderItem[] {
+    return this._items;
   }
 
   validate(): boolean {
     if (this._id.length === 0) {
       throw new Error("Id is required");
-    } 
+    }
     if (this._customerId.length === 0) {
       throw new Error("CustomerId is required");
     }
     if (this._items.length === 0) {
       throw new Error("Items are required");
     }
-    return true
+    return true;
   }
 
   total(): number {
